8e8cb127d882d65b031caf87cbb8cc8a39e1073b
Test <test@test.com>
2026-06-30 16:35:14 +0300

    Initial commit

---
+++ src/main/java/ro/upt/Car.java
@@ -0,0 +1,5 @@
+package ro.upt;
+
+public class Car {
+    private int totalKm;
+}
---
+++ src/test/java/ro/upt/MainTest.java
@@ -0,0 +1,12 @@
+package ro.upt;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+	@Test
+	public void testExample() {
+		int x = 10/5;
+		Assertions.assertEquals(2, x);
+	}
+}
